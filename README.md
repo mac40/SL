7fd63e28418649c6c3edfe6b8b4c35dd651e32ca
mac40 <marcobgg4@gmail.com>
2019-06-19 13:16:17 +0200

    game_parser/duplicates_handler

---
+++ README.md
@@ -0,0 +1,13 @@
+# Statistical Learning Project
+
+This is the final project for the course of Statistical Learing at University of Padua.
+
+The project consist of three steps:
+
+* Gathering data
+* analyzing the gathered data
+* drawing conclusions from the analysis
+
+## Credits
+
+* [MarcusDEFGH](https://github.com/mac40/loldraft) from which i took insipiration for the spider
--- game_parser.py
+++ game_parser.py
@@ -1,11 +1,28 @@
-'''
+"""
 This file is meant to parse the output from the spider in order to prepare various dataset to be
 analyzed with R
-'''
+"""
 
 import pandas as pd
+
+def teams_to_list(games):
+    """The first action 
+    """
+
+def duplicates_handler(games):
+    """In order to avoid unwanted interpreations of data we wish to remove
+    duplicate games, i.e. games gathered twice from two different player
+    since they were involved in the same game
+    :param games: the list of games
+    """
+    return games.drop_duplicates(subset="timestamp")
 
 
 if __name__ == "__main__":
     # import games from games.csv
     games = pd.read_csv('lolgames/games.csv')
+    
+    # clear games from duplicates
+    games = duplicates_handler(games)
+
+    # games["team_1"] = games["team_1"].apply(lambda x: x.split(","))
