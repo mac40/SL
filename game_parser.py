884ed51b31f0054e9a52baa0541cb25e05e993ba
mac40 <marcobgg4@gmail.com>
2019-06-20 10:36:24 +0200

    updated behavior of teams_to_list

--- game_parser.py
+++ game_parser.py
@@ -7,31 +7,55 @@ import pandas as pd
 
 
 def duplicates_handler(games):
-    """In order to avoid unwanted interpreations of data we wish to remove
+    """
+    In order to avoid unwanted interpreations of data we wish to remove
     duplicate games, i.e. games gathered twice from two different player
     since they were involved in the same game
+
     :param games: the list of games
     """
     return games.drop_duplicates(subset="timestamp")
 
 
 def teams_to_list(games):
-    """ In order to work more easily with teamcomps we will change their
+    """
+    In order to work more easily with teamcomps we will change their
     field from a csv style into a python list
+
     :param games: the list of games
     """
     games["team_1"] = games["team_1"].apply(lambda x: x.split(","))
+    games[["top_1", "jung_1", "mid_1", "adc_1", "supp_1"]] = pd.DataFrame(
+        games.team_1.values.tolist(), index=games.index)
+    games = games.drop(columns="team_1")
     games["team_2"] = games["team_2"].apply(lambda x: x.split(","))
+    games[["top_2", "jung_2", "mid_2", "adc_2", "supp_2"]] = pd.DataFrame(
+        games.team_2.values.tolist(), index=games.index)
+    games = games.drop(columns="team_2")
+    return games
+
+
+def change_date_format(games):
+    """
+    To allow time operations in R we need to change date format
+
+    :param games: the list of games
+    """
+    # games["timestamp"] = games["timestamp"].apply(lambda x: x[:10])
+    games["timestamp"] = pd.to_datetime(games["timestamp"])
     return games
 
 if __name__ == "__main__":
     # import games from games.csv
-    games = pd.read_csv('lolgames/games.csv')
-    
+    GAMES = pd.read_csv('lolgames/games.csv')
+
     # clear games from duplicates
-    games = duplicates_handler(games)
+    GAMES = duplicates_handler(GAMES)
 
     # reformat teams
-    games = teams_to_list(games)
+    GAMES = teams_to_list(GAMES)
 
-    print(games.head())
+    #reformat dates
+    GAMES = change_date_format(GAMES)
+
+    print(GAMES.head())
